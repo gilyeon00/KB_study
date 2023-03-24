ed850a36e8e808ad404f69021eb3c6f9aa50e4d7
gilyeon00 <12venus08@naver.com>
2023-03-24 14:34:25 +0900

    Array 사용해서 객체 생성 및 출력

--- work01_java/java02_Injection/com/edu/test/EmployeeAppTest1.java
+++ work01_java/java02_Injection/com/edu/test/EmployeeAppTest1.java
@@ -14,34 +14,23 @@ import com.edu.vo.Secratary;
  */
 public class EmployeeAppTest1 {
     public static void main(String[] args) {
-//        Manager manager = new Manager();    // 생성자 호출
-        Manager manager1 = new Manager("James", new MyDate(2000, 12, 8), 70000.0, "it");
-//
-//        // manager.name = "Gosling";    // private 이라 접근 안됨
-//        manager.setManagerField("Gosling", new MyDate(2000, 1, 10), 3000, "Marketing");
-//
-//        System.out.println(manager.getDetails());
-//        System.out.println(manager1.getDetails());
-//
-//        manager.getDetails();
+        Manager[] managers = {
+                new Manager("James", new MyDate(2000, 12, 8), 70000.0, "it"),
+                new Manager("Petter", new MyDate(1990, 2, 9), 21212.2, "Marketing"),
+                new Manager("Kim", new MyDate(2010, 11, 21), 41415.2, "it")
+        };
 
         Enginner eg = new Enginner("gg", new MyDate(2000, 12, 8), 55000, "Full", 8888);
         Secratary s = new Secratary("ss", new MyDate(1995, 8, 23), 45000, "gg");
 
         System.out.println("=========정보 출력============");
-        System.out.println(manager1.getDetails());
-        System.out.println(eg.getDetails());
-        System.out.println(s.getDetails());
+        for(Manager m : managers) System.out.println(m.getDetails());
 
         System.out.println("=========이름 출력============");
-        System.out.println(manager1.getName());
-        System.out.println(eg.getName());
-        System.out.println(s.getName());
+        for(Manager m : managers) System.out.println(m.getName());
 
         System.out.println("=========연봉 출력============");
-        System.out.println(manager1.getSalary() * 12);
-        System.out.println(eg.getSalary() * 12 + eg.getBonus());
-        System.out.println(s.getSalary() * 12);
+        for (Manager m : managers) System.out.println(m.getSalary() * 12 + "달러");
 
     }
 }
